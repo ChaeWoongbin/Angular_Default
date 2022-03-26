9a06dc1e600b6683e6376cbf8df254b7f2172a0d
jerpi94 <jerpi053@gmail.com>
2022-03-26 14:09:04 +0900

    기본 틀(navbar), 사이드바 메뉴

--- DefaultForm/src/app/app-routing.module.ts
+++ DefaultForm/src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './intro/intro.component';
 import { ListComponent } from './list/list.component';
 import { AboutComponent } from './about/about.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // 
   { path: 'home', component: HomeComponent, }, // 
-  { path: 'list', component: ListComponent, }, // 
   { path: 'intro', component: IntroComponent, }, // 
+  { path: 'joinnow', component: JoinnowComponent, }, // 
   { path: 'about', component: AboutComponent, }, //
+
+  
+  { path: 'list', component: ListComponent, }, // 
+
   { path: '**', redirectTo: '/home', pathMatch: 'full' }, // 
 ];
 
