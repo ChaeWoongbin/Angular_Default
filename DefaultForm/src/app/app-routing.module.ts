2f98acbb4169f91f53e53d8b98c08515e7f543f8
jerpi94 <jerpi053@gmail.com>
2022-03-31 11:27:46 +0900

    보조라우팅 작성중

--- DefaultForm/src/app/app-routing.module.ts
+++ DefaultForm/src/app/app-routing.module.ts
@@ -5,17 +5,25 @@ import { IntroComponent } from './intro/intro.component';
 import { ListComponent } from './list/list.component';
 import { AboutComponent } from './about/about.component';
 import { JoinnowComponent } from './joinnow/joinnow.component';
+import { ListDashboardComponent } from './list/list-dashboard/list-dashboard.component';
+import { ListHomeComponent } from './list/list-home/list-home.component';
+import { ListOrdersComponent } from './list/list-orders/list-orders.component';
+import { ListProductsComponent } from './list/list-products/list-products.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // 
-  { path: 'home', component: HomeComponent, }, // 
+  { path: 'home', component: HomeComponent,  }, // 
   { path: 'intro', component: IntroComponent, }, // 
-  { path: 'joinnow', component: JoinnowComponent, }, // 
+  { path: 'joinnow', component: JoinnowComponent,  }, // 
   { path: 'about', component: AboutComponent, }, //
 
   
   { path: 'list', component: ListComponent, }, // 
+  { path: 'dashboard', component: ListDashboardComponent, outlet: "sidebar" , }, // 
+  { path: 'home', component: ListHomeComponent, outlet: "sidebar" , }, // 
+  { path: 'orders', component: ListOrdersComponent, outlet: "sidebar" , }, // 
+  { path: 'products', component: ListProductsComponent, outlet: "sidebar" , }, // 
 
   { path: '**', redirectTo: '/home', pathMatch: 'full' }, // 
 ];
